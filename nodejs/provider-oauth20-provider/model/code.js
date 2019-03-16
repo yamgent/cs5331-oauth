369e0c7bddaa628c174c1878ba63faf40953269b
Tan Wang Leng <tanwangleng@outlook.com>
2019-03-16 17:14:12 +0800

    Store issued codes

--- nodejs/provider-oauth20-provider/model/code.js
+++ nodejs/provider-oauth20-provider/model/code.js
@@ -1,9 +1,16 @@
 const crypto = require('crypto');
 
+const savedCodes = [];
+
+const codeAliveDurationMs = 60 * 60 * 1000;
+
 const createCodeForUserWithClient = (userId, clientId, scope, ttl, callback) => {
     var code = crypto.randomBytes(32).toString('hex');
 
-    // TODO: Must save code somewhere!
+    // save codes so that relying party can verify with us
+    const newCode = { code, userId, clientId, scope, ttl: new Date().getTime() + codeAliveDurationMs};
+    console.log(newCode);
+    savedCodes.push(newCode);
 
     callback(null, code);
 };
